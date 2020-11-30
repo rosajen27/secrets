978910e9e97cc410b8701c66d048a6a39801a417
Jennifer Rosa <rosajen27@gmail.com>
2020-11-29 20:03:41 -0500

    Render Webpages

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
+// Render Webpages
+app.get("/", function (req, res) {
+    res.render("home");
+});
+
+app.get("/login", function (req, res) {
+    res.render("login");
+});
+
+app.get("/register", function (req, res) {
+    res.render("register");
+});
+
+
 app.listen(3000, function () {
     console.log("Server started on port 3000.");
 });
