f2a5e0effc70182ca97353614712f1a6e13713c1
Jennifer Rosa <rosajen27@gmail.com>
2020-12-12 17:24:51 -0500

    update callbackURL for Google OAuth

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ app.use(passport.session());
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
-    callbackURL: "http://localhost:3000/auth/google/secrets",
+    callbackURL: "http://rosa-secrets.herokuapp.com/auth/google/secrets",
     userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
 },
     function (accessToken, refreshToken, profile, cb) {
